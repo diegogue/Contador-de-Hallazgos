4abff287e924bc1d8b600458a8af79ada0a0b35c
diegogue <32721354+diegogue@users.noreply.github.com>
2018-06-02 23:45:35 -0600

    Create CellCountGUI.java

---
+++ CellCountGUI.java
@@ -0,0 +1,155 @@
+package ecci.GoF;
+
+import ij.ImagePlus;
+import ij.io.Opener;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Interfaz principal del programa contador de celulas.
+ */
+public class CellCountGUI implements Observer {
+    private JPanel mainPanel;
+    private JPanel counterPanel;
+    private JPanel actionsPanel;
+    private JButton openButton;
+    private JCheckBox box3;
+    private JCheckBox box4;
+    private JCheckBox box2;
+    private JCheckBox box1;
+    private JCheckBox box0;
+    private JLabel type0;
+    private JLabel type1;
+    private JLabel type2;
+    private JLabel type3;
+    private JLabel type4;
+    private JTextField nameChangeField;
+    private JLabel nameChange;
+    private JButton saveButton;
+    private JButton queryButton;
+    private JPanel databasePane;
+    private JLabel selectedLabel;
+    private JCheckBox selectedBox;
+    private ImagePlus image;
+    private CellCountImageCanvas imageCanvas;
+
+    /**
+     * Constructor de CellCountGUI.
+     * Inicializa los atributos y agrega los listeners correspondientes
+     */
+    private CellCountGUI() {
+        openButton.addActionListener(e -> {
+            Opener open = new Opener();
+            image = open.openImage("");
+            initializeImage();
+        });
+        box0.addActionListener(e -> selectLabel(0));
+        box1.addActionListener(e -> selectLabel(1));
+        box2.addActionListener(e -> selectLabel(2));
+        box3.addActionListener(e -> selectLabel(3));
+        box4.addActionListener(e -> selectLabel(4));
+        selectedLabel = type0;
+        selectedBox = box0;
+        nameChangeField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    selectedBox.setText(nameChangeField.getText());
+                }
+            }
+        });
+        queryButton.addActionListener(e -> {
+            JOptionPane.showMessageDialog(null,"No implementado",
+                    "No implementado", JOptionPane.INFORMATION_MESSAGE);
+        });
+        saveButton.addActionListener(e -> {
+            JOptionPane.showMessageDialog(null,"No implementado",
+                    "No implementado", JOptionPane.INFORMATION_MESSAGE);
+
+        });
+    }
+
+    /**
+     * Metodo principal de CellCountGUI.
+     * @param args no utilizado
+     */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("CellCountGUI");
+        frame.setContentPane(new CellCountGUI().mainPanel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    /**
+     * Inicializa el contador y abre la imagen
+     */
+    private void initializeImage() {
+        type0.setText("0");
+        type1.setText("0");
+        type2.setText("0");
+        type3.setText("0");
+        type4.setText("0");
+        if (imageCanvas != null) {
+            imageCanvas.close();
+            imageCanvas = null;
+        }
+        if (image != null) {
+            imageCanvas = new CellCountImageCanvas(image);
+            imageCanvas.addObserver(this);
+        }
+        box0.doClick();
+    }
+
+    /**
+     * Cambia la variable seleect label por type-n
+     * @param n indice de label
+     */
+    private void selectLabel(int n) {
+        switch (n) {
+            case 0:
+                selectedLabel = type0;
+                selectedBox = box0;
+                break;
+            case 1:
+                selectedLabel = type1;
+                selectedBox = box1;
+                break;
+            case 2:
+                selectedLabel = type2;
+                selectedBox = box2;
+                break;
+            case 3:
+                selectedLabel = type3;
+                selectedBox = box3;
+                break;
+            case 4:
+                selectedLabel = type4;
+                selectedBox = box4;
+                break;
+        }
+        if(imageCanvas != null) {
+            imageCanvas.selectType(n);
+        }
+    }
+
+    /**
+     * Metodo que es parte del patron de observer.
+     * Es llamado cuando se actualiza los objectos observados.
+     * Actualiza el contador de puntos.
+     * @param o   Objeto observado
+     * @param arg Argumentos enviados por el objecto observado
+     */
+    @Override
+    public void update(Observable o, Object arg) {
+        Integer count = imageCanvas.getPointCount();
+        selectedLabel.setText(count.toString());
+    }
+}
