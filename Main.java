6b6ee5bb057b69ef62390f783cad24f08a62c4de
diegogue <32721354+diegogue@users.noreply.github.com>
2018-06-02 23:34:04 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package ecci.GoF;
+
+/**
+ * Cell counter es un programa que implementa un simple contador
+ * de celulas
+ *
+ * @autor   Group of Five
+ * @version 0.0.4
+ * @since   2018-05
+ */
+public class Main {
+    public static void main(String[] args) {
+        CellCountGUI.main(args);
+    }
+}
