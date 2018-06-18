0291984ddcabf6872903f78d712d3644bd9f4ef4
jcose <jcose13@gmail.com>
2018-06-18 14:12:57 -0600

    cambios en loginGui

---
+++ src/gui/controllers/LoginController.java
@@ -0,0 +1,21 @@
+package gui.controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable{
+
+    @FXML
+    private Button enterLogin;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
