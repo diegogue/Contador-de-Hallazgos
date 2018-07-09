830c03cc5ca9901581aa460ae9bd924489f8235a
b22539 <sergiofv55@gmail.com>
2018-07-09 15:44:05 -0600

    Modificada la posicion de los botones, interfaz aun no lista

---
+++ src/main/java/ecci/GoF/EditWindow.java
@@ -0,0 +1,10 @@
+package ecci.GoF;
+
+import javax.swing.*;
+
+public class EditWindow {
+    private JTextField textField1;
+    private JButton editarNombreButton;
+    private JButton editarColorButton;
+    private JButton cerrarButton;
+}
